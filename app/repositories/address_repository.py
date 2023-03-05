80af19a5002698717e31ad485c8f3a4b17ca94aa
Guilherme Costa <guicm007@gmail.com>
2023-03-05 01:46:49 -0300

    feat: create address repository w/ functions

---
+++ app/repositories/address_repository.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models.address import Address
+
+class AddressRepository:
+    @staticmethod
+    def save(db: Session, address: Address) -> none:
+        if address.id:
+            db.merge(address)
+        else:
+            db.add(address)
+        db.commit()
+
+    @staticmethod
+    def find_by_id(db: Session, id: int) -> Address:
+        return db.query(Address).filter(Address.id == id).first()
