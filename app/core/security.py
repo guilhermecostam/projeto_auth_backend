e3d3d26dbacb33338fc522c9e842adfb303682cc
Guilherme Costa <guicm007@gmail.com>
2023-03-06 23:35:16 -0300

    feat: add jwt logic

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,13 @@
+import jwt
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
 from passlib.context import CryptContext
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app.models.user import User
+from app.core.settings import settings
+from app.repositories.user_repository import UserRepository
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +16,51 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+def authenticate_user(db: Session, username: str, password: str) -> bool | User:
+    user = UserRepository.find_by_username(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+def create_access_token(*, data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.MINUTES_TO_EXPIRE)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
+
+def decode_access_token(db, token):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(email=email)
+    except PyJWTError:
+        raise credentials_exception
+    user = crud.get_user_by_email(db, email=token_data.email)
+    if user is None:
+        raise credentials_exception
+
+    return user
